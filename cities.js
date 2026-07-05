0e816fb07c295792ce20a45a2e8f4b080de8b1e5
Sreehari Annam <sannam@wescodist.com>
2026-07-05 07:27:40 -0400

    batch 7: next cities

--- cities.js
+++ cities.js
@@ -1,20 +1,8 @@
 const newJerseyCities = [
-  "Saddle River", "Salem", "Sayreville", "Scotch Plains", "Sea Girt",
-  "Sea Isle City", "Seaside Heights", "Seaside Park", "Secaucus", "Sewaren",
-  "Sewell", "Short Hills", "Shrewsbury", "Sicklerville", "Skillman",
-  "Somerdale", "Somers Point", "Somerset", "Somerville", "South Amboy",
-  "South Bound Brook", "South Orange", "South Plainfield", "South River", "Sparta",
-  "Spotswood", "Spring Lake", "Springfield", "Stanhope", "Stanton",
-  "Stewartsville", "Stirling", "Stockholm", "Stockton", "Stone Harbor",
-  "Stratford", "Succasunna", "Summit", "Sussex", "Swedesboro",
-  "Teaneck", "Tenafly", "Test", "Teterboro", "Thorofare",
-  "Three Bridges", "Titusville", "Toms River", "Totowa", "Towaco",
-  "Trenton", "Tuckerton", "Union", "Union City", "United States",
-  "Vauxhall", "Ventnor City", "Vernon", "Verona", "Vincentown",
-  "Vineland", "Voorhees", "Waldwick", "Wallington", "Wanaque",
-  "Waretown", "Warren", "Washington", "Washington Township", "Watchung",
-  "Waterford Works", "Wayne", "Weehawken", "Wenonah", "West Berlin",
-  "West Creek", "West Long Branch", "West Milford", "West New York", "West Orange",
+  "Westfield", "Westville", "Westwood", "Wharton", "Whippany",
+  "Whitehouse", "Whitehouse Station", "Wildwood", "Williamstown", "Willingboro",
+  "Windsor", "Wood Ridge", "Woodbine", "Woodbridge", "Woodcliff Lake",
+  "Woodstown", "Wrightstown", "Wyckoff",
 ];
 
 const BATCH_SIZE = 5;
